5252c033987406e5c94d29fa854f4549810640ef
Alexey Luchko <luch@airdog.com>
2015-09-08 16:49:37 +0300

    [#101333490] disable rarely or never used modiles.

--- makefiles/config_AirDogFMU_common.mk
+++ makefiles/config_AirDogFMU_common.mk
@@ -14,14 +14,10 @@ ROMFS_EXTRA_FILES = $(PX4_BASE)/Images/px4io-v2_default.bin
 # IO Chip -- AirDog custom firmware
 #ROMFS_EXTRA_FILES = $(PX4_BASE)/Images/AirDogIO_default.bin
 
-# With custom SiK firmware
-#ROMFS_EXTRA_FILES += $(wildcard $(PX4_BASE)/ROMFS/FW/*)
-
 #
 # Board support modules
 #
 MODULES		+= drivers/bluetooth21
-MODULES		+= drivers/bluetooth21/tests/bluetooth21_devtest
 MODULES		+= drivers/boards/AirDogFMU/$(CONFIG_BOARD_REVISION)
 MODULES		+= drivers/boards/AirDogFMU/frame_button
 MODULES		+= drivers/calibration
@@ -31,7 +27,6 @@ MODULES		+= drivers/gps
 MODULES		+= drivers/hmc5883spi
 MODULES		+= drivers/l3gd20
 MODULES		+= drivers/led
-MODULES		+= drivers/ll905
 MODULES		+= drivers/lsm303d
 MODULES		+= drivers/mpu6000
 MODULES		+= drivers/ms5611
@@ -41,26 +36,21 @@ MODULES		+= drivers/stm32/adc
 MODULES		+= drivers/stm32/tone_alarm
 MODULES		+= modules/airdog
 MODULES		+= modules/bgc
-MODULES		+= modules/cl_helper
 MODULES		+= modules/debug_button
 MODULES		+= modules/gpio_tool
 MODULES		+= modules/eparam
-MODULES		+= modules/fs_test
 MODULES		+= modules/indication
-MODULES		+= modules/kbd_test
 MODULES		+= modules/sensors
-MODULES		+= modules/sensors_probe
-MODULES		+= modules/sensors_switch
-MODULES		+= modules/spi_exchange
-MODULES		+= modules/quick_log
-MODULES 	+= drivers/pwm_input
+MODULES		+= drivers/pwm_input
 #MODULES		+= drivers/airspeed
 #MODULES		+= drivers/blinkm
+#MODULES		+= drivers/bluetooth21/tests/bluetooth21_devtest
 #MODULES		+= drivers/ets_airspeed
 #MODULES		+= drivers/hil
 #MODULES		+= drivers/hmc5883
 #MODULES		+= drivers/hott/hott_sensors
 #MODULES		+= drivers/hott/hott_telemetry
+#MODULES		+= drivers/ll905
 #MODULES		+= drivers/mb1230serial
 #MODULES		+= drivers/mb12xx
 #MODULES		+= drivers/meas_airspeed
@@ -69,18 +59,13 @@ MODULES 	+= drivers/pwm_input
 #MODULES		+= drivers/px4fmu
 #MODULES		+= drivers/rgbled
 #MODULES		+= drivers/sf0x
-#MODULES		+= modules/SiKUploader
 
 #
 # System commands
 #
 MODULES		+= modules/airdog/calibrator
-MODULES		+= modules/bt_cfg
-MODULES		+= systemcmds/bl_update
-MODULES		+= systemcmds/boardinfo
 MODULES		+= systemcmds/config
 MODULES		+= systemcmds/dumpfile
-MODULES		+= systemcmds/esc_calib
 MODULES		+= systemcmds/mixer
 MODULES		+= systemcmds/mtd
 MODULES		+= systemcmds/nshterm
@@ -91,8 +76,22 @@ MODULES		+= systemcmds/pwm
 MODULES		+= systemcmds/reboot
 MODULES		+= systemcmds/top
 MODULES		+= systemcmds/ver
-MODULES		+= systemcmds/writefile
+#MODULES		+= modules/SiKUploader
+#MODULES		+= modules/bt_cfg
+#MODULES		+= modules/cl_helper
+#MODULES		+= modules/fs_test
+#MODULES		+= modules/kbd_test
+#MODULES		+= modules/sensors_probe
+#MODULES		+= modules/sensors_switch
+#MODULES		+= modules/serial_echo
+#MODULES		+= modules/serial_measure_latency
+#MODULES		+= modules/spi_exchange
+#MODULES		+= modules/stty
+#MODULES		+= systemcmds/bl_update
+#MODULES		+= systemcmds/boardinfo
+#MODULES		+= systemcmds/esc_calib
 #MODULES		+= systemcmds/tests
+#MODULES		+= systemcmds/writefile
 
 #
 # General system control
@@ -107,11 +106,11 @@ MODULES		+= modules/navigator
 #
 # Estimation modules (EKF/ SO3 / other filters)
 #
-MODULES		+= modules/attitude_estimator_ekf
-MODULES		+= modules/position_estimator_inav
 MODULES		+= modules/attitude_estimator_q
+MODULES		+= modules/position_estimator_inav
 #MODULES		+= examples/flow_position_estimator
 #MODULES		+= modules/airdog/trajectory_calculator
+#MODULES		+= modules/attitude_estimator_ekf
 #MODULES		+= modules/attitude_estimator_so3
 #MODULES		+= modules/ekf_att_pos_estimator
 
@@ -124,6 +123,7 @@ MODULES		+= modules/mc_pos_control
 #
 # Logging
 #
+MODULES		+= modules/quick_log
 MODULES		+= modules/sdlog2
 
 #
@@ -144,47 +144,18 @@ MODULES		+= modules/uORB
 #
 # Libraries
 #
+LIBRARIES	+= lib/mathlib/CMSIS
 MODULES		+= lib/activity
 MODULES		+= lib/airdog/hwinfo
-MODULES		+= lib/stm32f4
 MODULES		+= lib/conversion
 MODULES		+= lib/geo
 MODULES		+= lib/geo_lookup
 MODULES		+= lib/mathlib
 MODULES		+= lib/mathlib/math/filter
-LIBRARIES	+= lib/mathlib/CMSIS
+MODULES		+= lib/stm32f4
 #MODULES		+= lib/ecl
 #MODULES		+= lib/external_lgpl
 #MODULES		+= lib/launchdetection
-
-#
-# OBC challenge
-#
-#MODULES		+= modules/bottle_drop
-
-#
-# Demo apps
-#
-#MODULES		+= examples/math_demo
-# Tutorial code from
-# https://pixhawk.ethz.ch/px4/dev/hello_sky
-#MODULES		+= examples/px4_simple_app
-
-# Tutorial code from
-# https://pixhawk.ethz.ch/px4/dev/daemon
-#MODULES		+= examples/px4_daemon_app
-
-# Tutorial code from
-# https://pixhawk.ethz.ch/px4/dev/debug_values
-#MODULES		+= examples/px4_mavlink_debug
-
-# Tutorial code from
-# https://pixhawk.ethz.ch/px4/dev/example_fixedwing_control
-#MODULES		+= examples/fixedwing_control
-
-# Hardware test
-#MODULES		+= examples/hwtest
-#MODULES		+= modules/serial_echo
 
 #
 # Transitional support - add commands from the NuttX export archive.
